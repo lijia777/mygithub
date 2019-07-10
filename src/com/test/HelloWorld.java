cb2a1f1e55e99ad00529a7a32b840de64b339533
lijia777 <844738700@qq.com>
2019-07-10 20:22:34 +0800

    你好,世界

--- src/com/test/HelloWorld.java
+++ src/com/test/HelloWorld.java
@@ -7,5 +7,7 @@ package com.test;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("helloWorld");
+        System.out.println("123");
+        System.out.println("123");
     }
 }
