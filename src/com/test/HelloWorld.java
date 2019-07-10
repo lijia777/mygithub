5a9103ea3e8fd6a70ad4ead9b18ac487a9c74d2e
lijia777 <844738700@qq.com>
2019-07-10 20:06:52 +0800

    你好,世界

---
+++ src/com/test/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.test;
+
+/***
+ *@Author : Le
+ *@CreatDate : 2019/7/10 0010
+ ***/
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("helloWorld");
+    }
+}
